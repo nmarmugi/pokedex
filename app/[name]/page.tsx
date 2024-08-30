58b29f8fa91f046c599a815130830e3cc5f2fe54
Nicola Marmugi <marmu1@hotmail.it>
2024-08-30 17:58:45 +0200

    feat: esercitazione 30-08-24

--- app/[name]/page.tsx
+++ app/[name]/page.tsx
@@ -438,10 +438,11 @@ export default function Pokemon({params}: IPokemonName) {
 		setDescription(false)
 	}
 
-	if (!data) return <Loading />
-
 	return (
 		<div className="max-w-[768px] w-full h-[812px] bg-[#E85463] relative flex flex-col items-center pb-12">
+			{!data && <Loading />}
+			{data &&
+			<>
 			{data.cries.latest && <audio ref={audioRef} src={data.cries.latest}></audio>}
 			<Link className="absolute top-3 left-3 flex items-center gap-2 text-white font-semibold" href={'/'}><Image width={30} height={30} src='/img/button_2489325.svg' alt="Back to home"  />Home</Link>
 			<div className="absolute top-3 right-3 flex items-center gap-2">
@@ -491,6 +492,8 @@ export default function Pokemon({params}: IPokemonName) {
 					</>
 				)}
 			</div>
+			</>
+			}
 		</div>
 	)
 }
