42c49ae9d3a07eaa035fdae18dd1d413aab88f1c
Nicola Marmugi <marmu1@hotmail.it>
2024-08-30 17:51:53 +0200

    feat: esercitazione 30-08-24

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="max-w-[768px] w-full max-h-dvh h-dvh p-6 bg-[#E85463] relative">
+    <div className="max-w-[768px] w-full h-[812px] p-6 bg-[#E85463] relative">
       <div className="w-full flex items-center gap-2 mb-5">
         <Image width={40} height={40} src="/img/2lHyIcGNwCt73HvB4zwEyV9xtgt.svg" alt="Pokéball" />
         <h1 className="font-extrabold text-3xl text-[rgb(255,255,255)]">Pokédex</h1>
